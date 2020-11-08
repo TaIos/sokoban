68047e8c955db5c80bb0732e640007b094dc7073
Martin Šafránek <gismocz@gmail.com>
2020-11-08 19:01:51 +0100

    Update automated testing

--- pddl/test.sh
+++ pddl/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -rf plans/*
 while read solver; do
-	python ../downward/fast-downward.py --alias ${solver} --sas-file plans/${solver}.sas --search-time-limit 30m domain.pddl task.pddl
+	python ../downward/fast-downward.py --alias ${solver} --sas-file plans/${solver}.sas --search-time-limit 30m domain.pddl task.pddl > plans/${solver}.out 2>&1  &
 done <solvers_list.txt
